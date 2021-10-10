3568af9e22436beddcde014fe709b7b43c43836f
fullstackdev427 <bear_star713@hotmail.com>
2021-10-10 11:33:42 +0900

    Initial deployment of our empty pipeline

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -2,16 +2,17 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CdkpipelinesDemoStack } from '../lib/cdkpipelines-demo-stack';
+import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
 
 const app = new cdk.App();
-new CdkpipelinesDemoStack(app, 'CdkpipelinesDemoStack', {
+new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
@@ -21,5 +22,6 @@ new CdkpipelinesDemoStack(app, 'CdkpipelinesDemoStack', {
 
   
 });
+
 
 app.synth();
